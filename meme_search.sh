7dd252a81d3804b8ffeb34d8b4f85a6219ce7409
LeungHK <89854896+LeungHK@users.noreply.github.com>
2021-08-31 15:52:14 +0100

    Update meme_search.sh

--- meme_search.sh
+++ meme_search.sh
@@ -1,16 +1,11 @@
 #!/bin/bash --login
-#$ -l mem256
-#$ -pe smp.pe 16 # … reserves 16 cores
-#$ -j y
-#$ -o logs_researchproject2
 #$ -hold_jid search_spidroin.sh
 
-#qsub meme_search.sh ERR356424
 module load apps/anaconda3/5.2.0/bin
 source activate spider-motif
 
-cd /net/scratch2/a73590hl/data/$1
+cd /data/$1
 
-python /mnt/iusers01/sbc01/a73590hl/software/Spider-Pipeline/spider_pipeline_runner.py -mk spider_pipeline_$1
+python Spider-Pipeline/spider_pipeline_runner.py -mk spider_pipeline_$1
 
-python /mnt/iusers01/sbc01/a73590hl/software/Spider-Pipeline/spider_pipeline_runner.py -rme /net/scratch2/a73590hl/data/$1/spider_pipeline_$1 /net/scratch2/a73590hl/data/$1/aa_seq_sorted_match_$1.fasta -t 16
+python Spider-Pipeline/spider_pipeline_runner.py -rme /data/$1/spider_pipeline_$1 /data/$1/aa_seq_sorted_match_$1.fasta -t 16
